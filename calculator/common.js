c97fc30a246a14c1afef14c7b55417ffec6e6aa2
Dorabang <psa1195@naver.com>
2022-12-15 18:34:45 +0900

    calculator js update

--- calculator/common.js
+++ calculator/common.js
@@ -1,4 +1,4 @@
-const calculator = {
+/* const calculator = {
   add: function (a, b) {
     console.log(a + b);
   },
@@ -20,3 +20,36 @@ calculator.add(5, 8);
 calculator.minus(63, 18);
 calculator.multiplication(7, 19);
 calculator.power(2, 5);
+ */
+
+
+const calculator = {
+  add: function (a, b) {
+    return a + b;
+  },
+  minus: function(a, b){
+    return a - b;
+  },
+  divide: function(a, b){
+    return a / b;
+  },
+  multiplication: function(a, b){
+    return a * b;
+  },
+  power: function(a, b) {
+    return a ** b;
+  }
+};
+
+const addResult = calculator.add(5, 8);
+const minusResult = calculator.minus(63, addResult);
+const multiResult = calculator.multiplication(minusResult, 19);
+const powerResult = calculator.power(multiResult, 5);
+
+// console.log 사용 시에는 값을 저장하는 것이 아니라, 일시저장의 느낌이 강함. 일회성 값.
+// return 사용 시, console.log를 사용했을 때와 달리 값을 저장하여 보여주고 서로 상호연결하여 사용할 수 있음.
+
+console.log(addResult);
+console.log(minusResult);
+console.log(multiResult);
+console.log(powerResult);
