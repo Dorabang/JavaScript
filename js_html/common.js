ac6b6a3557a9535635f7b948dd53be9074014297
Dorabang <psa1195@naver.com>
2022-12-17 16:38:38 +0900

    html event

--- js_html/common.js
+++ js_html/common.js
@@ -1,17 +1,44 @@
 const title = document.querySelector(".title h1");
 
 function handleTitleClick() {
-  title.style.color = 'blue';
+    title.style.color = "blue";
 }
 
 function handleMouseEnter() {
-  title.innerText = "Mouse is here!";
+    title.innerText = "Mouse is here!";
 }
 
 function handleMouseLeave() {
-  title.innerText = "Mouse is gone!";
+    title.innerText = "Mouse is gone!";
 }
 
-title.addEventListener('click', handleTitleClick);
-title.addEventListener('mouseenter', handleMouseEnter);
-title.addEventListener('mouseleave', handleMouseLeave);
+function handleWindowResize() {
+  document.body.style.background = 'tomato';
+}
+
+function handleWindowCopy() {
+  alert('copier!');
+}
+
+function handleWindowOffline() {
+  alert('SOS no WIFI')
+}
+
+function handleWindowOnline() {
+  alert('All Good')
+}
+
+ // title.onclick = handleTitleClick;
+title.addEventListener("click", handleTitleClick);
+
+// title.onmouseenter = handleMouseEnter;
+title.addEventListener("mouseenter", handleMouseEnter);
+
+// title.onmoustleave = handleMouseLeave;
+title.addEventListener("mouseleave", handleMouseLeave); 
+
+
+window.addEventListener('resize', handleWindowResize);
+window.addEventListener('copy', handleWindowCopy);
+window.addEventListener('offline', handleWindowOffline);
+window.addEventListener('online', handleWindowOnline);
